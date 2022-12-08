4cbe3d9dce362a5da171bc885ece8c44263735c8
Sharon.Buch <buch.sharon@googlemail.com>
2022-12-08 10:10:18 +0100

    initial commit

---
+++ taschenrechner.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	  
+	float zahl1,zahl2;
+
+	printf("\nTaschenrechner\n\nGeben Sie die 1.Zahl ein: \n");
+
+	scanf("%f",&zahl1);
+
+	printf("\nGeben Sie die 2.Zahl ein: \n");
+	scanf("%f",&zahl2);
+
+
+	printf("\n\nDie Summe ergibt %f\n\n",zahl1+zahl2);
+
+	return 0;
+} 
